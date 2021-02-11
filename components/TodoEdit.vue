66bc1b3093354ac5bd83d370c6f0f917de0ac650
TsapkoZh <60929704+TsapkoZh@users.noreply.github.com>
2021-02-11 17:29:32 +0300

    Style TodoEdit

--- components/TodoEdit.vue
+++ components/TodoEdit.vue
@@ -38,3 +38,17 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+  .edit {
+    width: 81%;
+    height: 7.2vh;
+    font-family: arial, "Helvetica Neue", Helvetica, sans-serif;
+    font-weight: 500;
+    font-size: 1.05vw;
+    margin-left: 6vw;
+    padding: .5vw;
+    outline: none;
+    border: .1vw solid rgb(71, 114, 71);
+  }
+</style>
